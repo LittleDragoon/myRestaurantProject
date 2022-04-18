d4bec079567e40794c1406577fed20bb219cd1b1
LittleDragoon <benjamin1002@hotmail.fr>
2022-04-18 17:55:50 +0200

    Final adjustment
    Final Adjustment

--- server.js
+++ server.js
@@ -25,6 +25,19 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
+else {
+    app.get("/", (req, res) => {
+        res.send("Api running");
+    })
+}
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
