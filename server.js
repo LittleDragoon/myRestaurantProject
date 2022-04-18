6c163e7075d0fe4a22cb16e748150ca62731726e
LittleDragoon <benjamin1002@hotmail.fr>
2022-04-18 22:06:30 +0200

    e
    e

--- server.js
+++ server.js
@@ -20,14 +20,13 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 else {
     app.get("/", (req, res) => {
 
-
-        res.send("Api running");
+        res.send("Api is running");
     })
 }
 
